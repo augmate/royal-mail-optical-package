10d4312965b43e3b06ae0d7288553ac69be5e33a
Johny Urgiles <johny@augmate.com>
2014-08-08 12:21:27 -0400

    Rename cycle-counting -> CycleCounting

--- CycleCounting/src/main/java/com/augmate/cyclecounting/MainActivity.java
+++ CycleCounting/src/main/java/com/augmate/cyclecounting/MainActivity.java
@@ -1,4 +1,4 @@
-package com.augmate.cycle_counting;
+package com.augmate.cyclecounting;
 
 import android.app.Activity;
 import android.content.Intent;
--- CycleCounting/src/test/java/com/augmate/cycle_counting/ApplicationTest.java
+++ CycleCounting/src/test/java/com/augmate/cycle_counting/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.augmate.cycle_counting;
+package com.augmate.cyclecounting;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
